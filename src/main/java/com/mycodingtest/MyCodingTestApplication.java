b81fc70f63c4994b4449dec8b826b03fd8a2f257
Seonghun Jeong <tjdgns5506@gmail.com>
2024-12-27 19:14:29 +0900

    chore: 프로젝트 초기 커밋

---
+++ src/main/java/com/mycodingtest/MyCodingTestApplication.java
@@ -0,0 +1,13 @@
+package com.mycodingtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCodingTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyCodingTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mycodingtest/MyCodingTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycodingtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyCodingTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
