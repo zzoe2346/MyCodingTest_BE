1101dc6ec5edb63cbfef5afa414d1d90375dbcce
Seonghun Jeong <tjdgns5506@gmail.com>
2025-01-01 14:30:32 +0900

    feat: UserDetail 관련 로직 커스텀

---
+++ src/main/java/com/mycodingtest/security/CustomUserDetailService.java
@@ -0,0 +1,28 @@
+package com.mycodingtest.security;
+
+import com.mycodingtest.entity.User;
+import com.mycodingtest.repository.UserRepository;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import java.util.Collections;
+
+@Component
+public class CustomUserDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return new UserDetails(user.getId(), user.getUsername(), user.getPassword(), Collections.singleton(new SimpleGrantedAuthority(user.getRole())));
+    }
+}
---
+++ src/main/java/com/mycodingtest/security/UserDetails.java
@@ -0,0 +1,39 @@
+package com.mycodingtest.security;
+
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Collection;
+import java.util.List;
+
+public class UserDetails implements org.springframework.security.core.userdetails.UserDetails {
+    private final Long userId;
+    private final String username;
+    private final String password;
+    private final Collection<? extends GrantedAuthority> authorities;
+
+    public UserDetails(Long userId, String username, String password, Collection<? extends GrantedAuthority> authorities) {
+        this.userId = userId;
+        this.username = username;
+        this.password = password;
+        this.authorities = authorities;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+}
