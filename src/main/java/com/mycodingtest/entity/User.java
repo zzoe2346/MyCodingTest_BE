7fefa2650ff49de8f9b20820858af3ef062e4ed6
Seonghun Jeong <tjdgns5506@gmail.com>
2025-01-01 14:25:04 +0900

    feat: User 엔티티, 리포지토리 추가

---
+++ src/main/java/com/mycodingtest/entity/User.java
@@ -0,0 +1,51 @@
+package com.mycodingtest.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String email;
+    private String password;
+    private String role;
+
+    public User(String username, String password, String email, String role) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.role = role;
+    }
+
+    protected User() {
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
---
+++ src/main/java/com/mycodingtest/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.mycodingtest.repository;
+
+import com.mycodingtest.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+
+    boolean existsByEmail(String email);
+}
