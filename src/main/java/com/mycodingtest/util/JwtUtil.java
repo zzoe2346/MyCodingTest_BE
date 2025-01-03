23c1a087c6c0388dbd053f5670858d5edfd9458e
Seonghun Jeong <tjdgns5506@gmail.com>
2025-01-03 18:28:55 +0900

    refactor: JwtUtil 에서 secretKey, expiration 을 외부에서 주입받도록 수정
    - 보안 향상
    - 키 직접 관리

--- src/main/java/com/mycodingtest/util/JwtUtil.java
+++ src/main/java/com/mycodingtest/util/JwtUtil.java
@@ -1,6 +1,10 @@
 package com.mycodingtest.util;
 
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
@@ -8,19 +12,28 @@ import java.util.Date;
 
 @Component
 public class JwtUtil {
-    private static final SecretKey KEY = Jwts.SIG.HS256.key().build();
-    private static final int JWT_EXPIRATION_IN_MS = 1000 * 60 * 60 * 2;
+
+    @Value("${jwt.secret-key}")
+    private String plainSecretKey;
+    @Value("${jwt.expiration}")
+    private Long expiration;
+    private SecretKey secretKey;
+
+    @PostConstruct
+    public void init() {
+        this.secretKey = Keys.hmacShaKeyFor(Decoders.BASE64.decode(plainSecretKey));
+    }
 
     public String generateToken(String username) {
         return Jwts.builder()
                 .subject(username)
                 .issuedAt(new Date(System.currentTimeMillis()))
-                .expiration(new Date(System.currentTimeMillis() + JWT_EXPIRATION_IN_MS))
-                .signWith(KEY)
+                .expiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(secretKey)
                 .compact();
     }
 
     public String validateAndExtractUsername(String token) {
-        return Jwts.parser().verifyWith(KEY).build().parseSignedClaims(token).getPayload().getSubject();
+        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().getSubject();
     }
 }
